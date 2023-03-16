8a4ca9c85910e55cbfb407406794207f85e6f3df
oben-desmond <obend678@gmail.com>
2023-03-16 10:37:21 +0100

    added get method to reponse

--- src/index.ts
+++ src/index.ts
@@ -96,6 +96,22 @@ app.post("/ta/payment", cors(), async (req, res) => {
     res.send({ message: "Successfuly verified transactions", body: (req.body) });
 });
 
+app.get("/ta/verify", cors(), async (req, res) => {
+
+    const result = await verifyZitoPaymentSuccess(req.body.id)
+    // console.log({ message: "Successfuly registered prospects ", body: (req.body) })
+    if (result) res.send({ message: "verification successful", status: 1 });
+    else res.send({ message: "verification failed", status: -1 });
+});
+
+app.get("/ta/payment", cors(), async (req, res) => {
+
+    await validateTransactions()
+    // console.log({ message: "Successfuly registered prospects ", body: (req.body) })
+    res.send({ message: "Successfuly verified transactions", body: (req.body) });
+});
+
+
 
 
 app.get("/", cors(), (req, res) => {
